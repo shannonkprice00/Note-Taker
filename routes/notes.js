78ee3833a9a47a91730289c79fa318e44be8a43c
Shannon Price <124547956+shannonkprice00@users.noreply.github.com>
2023-05-03 12:20:32 -0500

    added code for MVP

---
+++ routes/notes.js
@@ -0,0 +1,45 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const {v4 : uuidv4} = require('uuid');
+
+// Get route for retrieving all note items
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received for note items`);
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// Post route for a new note
+notes.post('/', (req, res) => {
+    console.info(`${req.method} request recieved to add new note`);
+    console.log(req.body);
+
+    const { title, text } = req.body;
+
+    if(req.body) {
+        const newNote = {
+            title,
+            text,
+            id: uuidv4(),
+        };
+        readAndAppend(newNote, './db/db.json');
+        res.json(`Note added successfully 🚀`)
+    } else {
+        res.error('Error in adding note');
+    }
+});
+
+// Delete route to delete notes
+// notes.delete('/:id', (req, res) => {
+//     console.info(`${req.method} request recieved to delete note`);
+//     const id = req.params.id
+//     console.log(id);
+
+//     if(id != null) {
+//         readAndDelete(id, './db/db.json')
+//         res.json(`Note deleted successfully 🗑️`)
+//     } else {
+//         res.error('Error in deleting note');
+//     }
+// })
+
+module.exports = notes;
